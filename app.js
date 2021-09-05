da0f6f034a24c55446fb6a3bfb1df63a7734ff67
mariyasada <mariyasada2406@gmail.com>
2021-09-05 18:45:13 +0530

    adding function in js

---
+++ app.js
@@ -0,0 +1,29 @@
+var txtinput = document.querySelector("#text-input");
+var btntranslate = document.querySelector(".btn-translate");
+var txtoutput =document.querySelector("#text-output");
+
+var url="https://api.funtranslations.com/translate/ebonics.json";
+
+function constructUrl(text)
+{
+    return url + "?" + "text=" + text;
+}
+
+function errorHandler(){
+    console.log("error occured",error);
+    alert("something wrong with server! try again after some time");
+}
+
+btntranslate.addEventListener("click",() =>{
+
+    var inputText = txtinput.value;
+    console.log("heyyyyy")
+    fetch(constructUrl(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText=json.contents.translated;
+        txtoutput.innerText = translatedText;
+    })
+    .catch(errorHandler)
+});
+
